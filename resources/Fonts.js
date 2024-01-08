b3669a8b2e37c931a0f07c42295dc6e77b09829f
Raveen Sasidharan <raveen.sasidharan@techvedika.com>
2024-01-08 13:58:03 +0530

    assets are added

---
+++ resources/Fonts.js
@@ -0,0 +1,6 @@
+export const Fonts = {
+    'AvenirNextLTProRegular': require("../assets/fonts/AvenirNextLTProRegular.otf"),
+    'AvenirNextLTProMedium': require("../assets/fonts/AvenirNextLTProMedium.otf"),
+    'AvenirNextLTProBold': require("../assets/fonts/AvenirNextLTProBold.otf"),
+    'AvenirNextLTProDemi': require("../assets/fonts/AvenirNextLTProDemi.otf")
+};
